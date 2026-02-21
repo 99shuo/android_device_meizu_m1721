58ff02aca5d372dfbf0b396760b005d7800436d9
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-02-21 16:51:28 +0530

    Update kernel configuration and version in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
 
 # Kernel
-TARGET_KERNEL_CONFIG := m1721_defconfig
+TARGET_KERNEL_CONFIG := vendor/m1721_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=7 androidboot.usbconfigfs=true
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
@@ -53,6 +53,7 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_RAMDISK_USE_LZ4 := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/meizu/m1721
+TARGET_KERNEL_VERSION := 4.19
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
